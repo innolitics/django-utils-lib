c0b435eb45230f382fbbba82265d26d60e8d1cf4
Joshua Tzucker <joshua.tz@gmail.com>
2024-11-03 22:21:47 -0800

    Fix typing (add pydantic mypy plugin, add type)

--- django_utils_lib/lazy.py
+++ django_utils_lib/lazy.py
@@ -1,3 +1,6 @@
+from typing import Callable, Optional
+
+
 class LazyDjango:
     """
     Django has a few modules where if you import something from them,
@@ -8,6 +11,8 @@ class LazyDjango:
     This class is a workaround, without using a full app registration system,
     to just use lazy-imports.
     """
+
+    _redirect_to_login: Optional[Callable] = None
 
     @property
     def redirect_to_login(self):
