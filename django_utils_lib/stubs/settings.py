0c75b1dd308438151b189939eb071d46d8e19dc1
Joshua Tzucker <joshua.tz@gmail.com>
2024-11-11 20:14:21 -0800

    Fix django-stubs / mypy config

---
+++ django_utils_lib/stubs/settings.py
@@ -0,0 +1,13 @@
+# Warning: This is a stub `settings.py` file, to satisfy the django-stubs mypy
+# plugin
+
+
+INSTALLED_APPS = [
+    # Django internals
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+]
